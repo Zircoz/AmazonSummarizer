ca6bcd142cbb0b6dac5f988b44013b89b1fcd398
Zircoz <dixit.setu@gmail.com>
2022-10-31 00:31:07 +0530

    add downloader with js
    Co-authored-by: Shubham Sengupta <shubham_sen@srmuniv.edu.in>

--- filesDownloader/HTMLObjectParser.js
+++ filesDownloader/HTMLObjectParser.js
@@ -6,12 +6,14 @@
 2. Right click on the tbody element and click "use in console". A new object in console will be created, assign it to `rows` then run the following code in console.
 */
 
-//rows=temp1
-rowsInnerHTML = []
-for (let step = 1; step < rows.length; step++) {
-  rowsInnerHTML.push(rows[step].children[2].innerHTML)
+//rows=temp0
+const parser = new DOMParser();
+for (let step = 1; step < 5; step++) {
+  strInnerHTML = rows.children[step].children[2].innerHTML;
+  innerHTMLtemp = parser.parseFromString(strInnerHTML, "text/html");
+  downloadURL = innerHTMLtemp.getElementsByTagName("a")[0].href;
+  window.open(downloadURL);
 }
-rowsInnerHTML
 
 /* 
 Right click on output of command and select copy whole object, paste it to the object.json file.
